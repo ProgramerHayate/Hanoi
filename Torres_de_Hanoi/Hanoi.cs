74cc7ebe1d6f7813a3a3a63e37211e9c9b80df8c
DavidLopez <davidlopez.gandia@gmail.com>
2021-03-08 15:53:02 +0100

    Commit Hanoi

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,15 +8,89 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
+        int m = 0;
+        /*Implementación de metodos*/
         public void mover_disco(Pila a, Pila b)
         {
 
-        }
+            if ((a.Top > b.Top || a.Top == 0) && b.Top != 0)
+            {
+                a.push(b.pop());
+            }
+            else if ((b.Top > a.Top || b.Top == 0) && a.Top != 0)
+            {
+                b.push(a.pop());
+            }
+            m++;
 
+        }
+        /*Iterativo*/
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
-            return 0;
+
+            if (n % 2 == 0)
+            {
+                while (fin.Size < n)
+                {
+                    mover_disco(aux, ini);
+
+                    mover_disco(ini, fin);
+
+                    if (fin.Size == n)
+                    {
+                        return m;
+                    }
+
+                    mover_disco(fin, aux);
+
+                    if (fin.Size == n)
+                    {
+                        return m;
+                    }
+                }
+            }
+            if (n % 2 != 0 || n == 1)
+            {
+                while (fin.Size < n)
+                {
+                    mover_disco(ini, fin);
+
+                    if (fin.Size == n)
+                    {
+                        return m;
+                    }
+                    mover_disco(ini, aux);
+
+                    mover_disco(aux, fin);
+                    if (fin.Size == n)
+                    {
+                        return m;
+                    }
+
+                }
+            }
+            return m;
+        }
+
+        /*Recursivo*/
+        public int recursivo(int n, Pila ini, Pila fin, Pila aux)
+        {
+
+            if (n == 1)
+            {
+
+                mover_disco(ini, fin);
+
+            }
+            else
+            {
+                recursivo(n - 1, ini, aux, fin);
+                mover_disco(ini, fin);
+
+                recursivo(n - 1, aux, fin, ini);
+            }
+
+            return m;
         }
 
     }
